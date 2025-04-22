de1b4624f07cb19bce4a0009ef7caac15f410c12
HONEYBADGERILLA <honeybadgerilla3@gmail.com>
2025-04-22 21:31:09 +0300

    addedspeed,fullscreen,q to quit,movemennt left right with stop on edge, refactored key events

--- AlienInvationGame.py
+++ AlienInvationGame.py
@@ -21,7 +21,12 @@ class AlienInvation:
 
         self.settings=Setttings()              #settings class instance
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invation")    #window title
 
         self.ship = Ship(self)              #notice the self passed is the second self ie the game ai_game
@@ -50,16 +55,41 @@ class AlienInvation:
         """respond to keypress and mouse events"""
 
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:  #cloe window with x button
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    #move ship right
-                    self.ship.moving_right = True
+                self._check_keydown_events(event)
+               
                     
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
+                self._check_keyup_events(event)      
+                
+
+
+    def _check_keydown_events(self,event):
+        """respond to key press"""
+
+        if event.key == pygame.K_RIGHT:
+            #move ship right
+            self.ship.moving_right = True
+
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+
+    def _check_keyup_events(self,event):
+        """respond to key release"""
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
                 
 
 
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """init the ship and its first position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
 
         #Load ship and get its rect(hitbox)
@@ -16,14 +17,20 @@ class Ship:
 
         #start each new ship in bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         #movement flags,start with no motion
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """update ship position based on flags"""
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x=self.x
 
 
 
